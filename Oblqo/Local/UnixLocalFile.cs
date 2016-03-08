11996477ad436a7ed1ea161688d19ce8288c85cc
Denis Gukov <denguk@gmail.com>
2016-03-08 23:39:35 +0500

    feat: unix fs support

---
+++ Oblqo/Local/UnixLocalFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Oblqo.Local;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+namespace Oblqo
+{
+	public class UnixLocalFile : LocalFile
+	{
+		public UnixLocalFile(LocalDrive drive, FileSystemInfo file)
+			:base(drive, file)
+		{
+		}
+
+
+		public override string GetAttribute(string name)
+		{
+			byte[] bytes;
+			Mono.Unix.Native.Syscall.getxattr (FullName, name, out bytes);
+			if (bytes == null) {
+				return null;
+			}
+			return Encoding.UTF8.GetString (bytes);
+		}
+
+		protected override void SetAttribute(string name, string value)
+		{
+			Mono.Unix.Native.Syscall.setxattr (FullName, name, Encoding.UTF8.GetBytes(value));
+		}
+
+		public override async Task SetAttributeAsync(string name, string value, CancellationToken token)
+		{
+			await Task.Run(() => SetAttribute(name, value));
+		}
+
+
+
+	}
+}
+

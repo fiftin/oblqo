1dd96d49a6bf9180108fcb3f0b44f19ed33c8ec1
Denis Gukov <denguk@gmail.com>
2016-03-12 01:06:06 +0500

    feat: support mono and unix

--- Oblqo/Local/UnixLocalFile.cs
+++ Oblqo/Local/UnixLocalFile.cs
@@ -14,28 +14,31 @@ namespace Oblqo
 		{
 		}
 
-
 		public override string GetAttribute(string name)
 		{
 			byte[] bytes;
-			Mono.Unix.Native.Syscall.getxattr (FullName, name, out bytes);
+			var attr = FormatAttributeName (name);
+			Mono.Unix.Native.Syscall.getxattr(FullName, attr, out bytes);
 			if (bytes == null) {
 				return null;
 			}
-			return Encoding.UTF8.GetString (bytes);
+			var ret = Encoding.UTF8.GetString (bytes);
+			return ret;
 		}
 
 		protected override void SetAttribute(string name, string value)
 		{
-			Mono.Unix.Native.Syscall.setxattr (FullName, name, Encoding.UTF8.GetBytes(value));
+			Mono.Unix.Native.Syscall.setxattr(FullName, FormatAttributeName(name), Encoding.UTF8.GetBytes(value));
 		}
 
 		public override async Task SetAttributeAsync(string name, string value, CancellationToken token)
 		{
-			await Task.Run(() => SetAttribute(name, value));
+			await Task.Run(() => SetAttribute(FormatAttributeName(name), value));
 		}
 
-
+		private static string FormatAttributeName(string name) {
+			return string.Format ("user.{0}", name);
+		}
 
 	}
 }
